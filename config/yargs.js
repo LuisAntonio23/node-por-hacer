b0b51df4142789528d19000e50b44760a0585359
Luis <luis_antonio_de@outlook.es>
2018-09-28 16:26:57 -0500

    primer-commit

---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+
+}
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'borra un elemento de la lista por hacer', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
